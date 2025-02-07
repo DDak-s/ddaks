585e4bf85ef7769877c5593fab63ec889265a50b
jykjulie <jyk.figma@gmail.com>
2025-02-08 00:30:46 +0900

    add ImgUrl_headers, edit filename
    이미지 헤더 추가, 생성 파일명 수정, 수집항목 추가

--- kakao_emoticon.py
+++ kakao_emoticon.py
@@ -8,7 +8,7 @@ headers = {
     "Accept": "application/json"  # JSON 응답을 원한다고 명시
 }
 # 페이지들 로드
-urls = ['https://e.kakao.com/api/v1/items/t/baby-choonsik-2']  # 크롤링할 URL 리스트
+urls = ['https://e.kakao.com/api/v1/items/t/dyu-ganadi-2']  # 크롤링할 URL 리스트
 
 # 저장할 폴더 이름 설정 (폴더가 없다면 생성)
 save_folder = "kakao_emoticons"
@@ -23,6 +23,13 @@ for url in urls:
     # 제목
     title = data['result']['title']
     item_folder = os.path.join(save_folder, title.replace(" ", "_"))
+
+    # 상품url(상품영어이름), 작가ID
+    title_url = data['result']['titleUrl']
+    artistID = data['result']['creator']['id']
+    
+    # 비슷한 스타일 similarStyleItems
+    similarStyleItems = data['result']['similarStyleItems']
     
     #폴더가 이미 존재하면 건너뛰고 다음 URL로
     if os.path.exists(item_folder):
@@ -36,12 +43,12 @@ for url in urls:
 
 
     for idx, img_url in enumerate(thumbnail_urls):
-        file_name = os.path.join(item_folder, f"image_{idx+1}.jpg")
+        file_name = os.path.join(item_folder, f"image_{title_url}_{idx+1}.jpg")
         print(f"Downloading {img_url} to {file_name}")
 
         try:
             # 이미지 다운로드
-            img_data = requests.get(img_url).content
+            img_data = requests.get(img_url,headers=headers).content
             with open(file_name, "wb") as f:
                 f.write(img_data)
         except Exception as e:
@@ -55,14 +62,19 @@ for url in urls:
     #JSON에 저장할 데이터 구성
     item_data = {
         "title": title,
-        "tag": tag,
+        "title_url":title_url,
         "artist": artist,
+        "artistID": artistID,
+        "tag": tag,
+        "similarStyleItems":similarStyleItems,
         "image_urls": thumbnail_urls
     }
     
     result_data.append(item_data)
 
+
+
 # 크롤링한 데이터를 JSON 파일로 저장
-with open("kakao_emoticons_data.json", "w", encoding="utf-8") as json_file:
+with open(f"kakao_emoticons_data_{title_url}.json", "w", encoding="utf-8") as json_file:
     json.dump(result_data, json_file, ensure_ascii=False, indent=4)
         
